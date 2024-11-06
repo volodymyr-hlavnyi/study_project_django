469e8901624a373511a9146c4cdf087e152f2fe8
volodymyr.hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-11-06 08:32:20 +0100

    hw 9 Task Manager (continue)

--- task/admin.py
+++ task/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Task, Category, SubTask
+from .models import Task, SubTask, Category
 
-admin.site.register(Task)
-admin.site.register(Category)
-admin.site.register(SubTask)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'task')
+    search_fields = ('title', 'task__title')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
